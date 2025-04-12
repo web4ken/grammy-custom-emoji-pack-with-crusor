eb8d7cef27d112417fa504da7cbdf62e60122d2c
Anton Kenikh <kenikh@me.com>
2025-04-12 22:16:13 +0400

    feat: init bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
+import { Bot } from 'grammy';
+import { config } from 'dotenv';
+
+config();
+
 function main() {
-    console.log('Hello, TypeScript!');
+    const botToken = process.env.BOT_TOKEN
+
+    if (botToken == null) {
+        console.error('BOT_TOKEN is not set');
+        process.exit(1);
+    }
+
+    const bot = new Bot(botToken);
+
+    bot.on('message', async (ctx) => {
+        await ctx.reply('Hello, world!');
+    });
+
+    bot.start();
+    console.log('Bot started');
 }
 
 main(); 
